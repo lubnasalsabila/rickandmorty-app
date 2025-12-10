9636b0f45a5e3a81c8458609aed756a5773d69c7
lubna salsabila <sitilubnasalsabilamuslimah@smkwikrama.sch.id>
2025-12-10 16:15:39 +0700

    last push

--- src/app/character/[id]/page.jsx
+++ src/app/character/[id]/page.jsx
@@ -1,4 +1,6 @@
+export const runtime = 'edge';
 export const dynamic = "force-dynamic";
+
 
 import Link from "next/link";
 import Loading from "./loading";
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
